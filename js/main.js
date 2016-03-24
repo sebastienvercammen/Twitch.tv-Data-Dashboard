06807e669a1a8464819d2dfa027417a81e53b5fb
Sébastien Vercammen <contact@sebastienvercammen.be>
2016-03-24 23:25:40 +0100

    Added messages per minute per channel.

--- js/main.js
+++ js/main.js
@@ -38,6 +38,21 @@
 		return d + ' days, ' + h + ' hours, ' + m + ' minutes';
 	}
 	
+	function updateAllChannelCounters(msgsPerMinute) {
+		var els = document.getElementsByClassName('channel-row');
+		
+		for(var i = 0; i < els.length; i++) {
+			var channel = els[i].getElementsByClassName('channel')[0].textContent;
+			var elCount = els[i].getElementsByClassName('count')[0];
+			
+			if(msgsPerMinute.hasOwnProperty(channel)) {
+				elCount.textContent = msgsPerMinute[channel].length;
+			} else {
+				elCount.textContent = 0;
+			}
+		}
+	}
+	
 	socket.on('stats', function(data) {
 		var uptime = millisecondsToDays(data.uptime);
 		var emotesPerMinute = data.stats.emoteCountPerMinute;
@@ -51,6 +66,9 @@
 		document.getElementById('uptime').textContent = uptime;
 		document.getElementById('emotes-per-minute').textContent = countEmotesPerMinute;
 		document.getElementById('msgs-per-minute').textContent = countTotalMsgsPerMinute;
+		
+		// Update all channels
+		updateAllChannelCounters(chanMsgsPerMinute);
 	});
 	
 	socket.on('channels', function(data) {
@@ -60,18 +78,28 @@
 		
 		for(var i = 0; i < data.length; i++) {
 			var row = table.insertRow(-1);
+			row.className = 'channel-row';
 			
 			// Striped?
 			if((i + 1) % 2 !== 0) {
-				row.className = 'pure-table-odd';
+				row.className += ' pure-table-odd';
 			}
 			
 			var cellId = row.insertCell(-1);
+			cellId.className = 'id';
+			
 			var cellChannel = row.insertCell(-1);
+			cellChannel.className = 'channel';
+			
+			var cellCountMsgs = row.insertCell(-1);
+			cellCountMsgs.className = 'count';
+			
 			var cellLink = row.insertCell(-1);
+			cellLink.className = 'link';
 			
 			cellId.textContent = (i + 1);
 			cellChannel.textContent = data[i];
+			cellCountMsgs.textContent = 0;
 			cellLink.innerHTML = '<a href="http://twitch.tv/' + data[i].substr(1) + '" rel="nofollow"><i class="fa fa-twitch"></a>';
 		}
 	});
